5fd1012f2f9d509bb6e918061ceee7ea3fc34c3f
prabha129 <35652743+prabha129@users.noreply.github.com>
2018-01-21 15:51:37 +0530

    Update vowelorconsonent.c

--- vowelorconsonent.c
+++ vowelorconsonent.c
@@ -9,8 +9,8 @@ int main()
     isLowercaseVowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
     isUppercaseVowel = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
     if (isLowercaseVowel || isUppercaseVowel)
-        printf("%c is a vowel.", c);
+    printf("%c is a vowel.", c);
     else
-        printf("%c is a consonant.", c);
+    printf("%c is a consonant.", c);
     return 0;
 }
