a20634522499424909de42914c89651f8f467e14
prabha129 <35652743+prabha129@users.noreply.github.com>
2018-01-21 14:48:19 +0530

    Update positive.c

--- positive.c
+++ positive.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
 int main()
 {
-    double number;
-
-    printf("Enter a number: ");
+  double number;
+ printf("Enter a number: ");
     scanf("%lf", &number);
-
-    if (number <= 0.0)
+ if (number <= 0.0)
     {
         if (number == 0.0)
             printf("You entered 0.");
         else
-            printf("You entered a negative number.");
-    }
+            printf("You entered a negative number.");  }
     else
         printf("You entered a positive number.");
-    return 0;
-}
+    return 0;}
