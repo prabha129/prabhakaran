0e65605d3ed5611ebc168aafb9d1a5ace34c6c2e
prabha129 <35652743+prabha129@users.noreply.github.com>
2018-02-07 16:13:23 +0530

    Create primenum.c

---
+++ primenum.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+    int v, k, i, flag;
+    printf("Enter two numbers(intervals): ");
+    
+    
+    
+    scanf("%d %d", &v, &k);
+    printf("Prime numbers between %d and %d are: ", v, k);
+    while (v < k)
+    {
+        flag = 0;
+        for(i = 2; i <= v/2; ++i)
+        {
+            if(v % i == 0)
+            {
+                flag = 1;
+                break;
+            }
+        }
+        if (flag == 0)
+            printf("%d ",v);
+        ++v;
+    }
+    return 0;
+}
