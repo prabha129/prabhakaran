e5f434be74667221ee0f115ac22f7a887e54a82a
prabha129 <35652743+prabha129@users.noreply.github.com>
2018-02-07 16:10:34 +0530

    Create even.c

---
+++ even.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i,n;
+clrscr();
+printf("enter the number");
+
+
+
+scanf("%d",&n);
+for(i=0;i<=a;i++)
+{
+if(i%2==0)
+{
+printf("%d",i);
+}
+}
+getch();
+return 0;
+}
