c5bcf6ea0f3b559121a9a164eda4c26e55d0f134
prabha129 <35652743+prabha129@users.noreply.github.com>
2018-02-09 17:26:44 +0800

    Create 52.c

---
+++ 52.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() 
+{
+	int mins,hour=0;
+	scanf("%d",&mins);
+  
+  
+	if(mins<60)
+	{
+  
+	    printf("%d %d",hour,mins);
+      
+	}
+  
+	else 
+	{
+	    hour=mins/60;
+	    mins=mins%60;
+	    printf("%d %d",hour,mins);
+	}
+	return 0;
+}
