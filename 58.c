ff0c9c10c229a12cbb7af43879f52b4397b221cc
prabha129 <35652743+prabha129@users.noreply.github.com>
2018-02-12 16:05:40 +0530

    Create 58.c

---
+++ 58.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+
+	char str[50];
+  
+	int c=0,i;
+  
+    scanf("%[^\n]s",str);
+    
+    int l;
+    
+    l=strlen(str);
+    
+	for (i = 0; i<l; i++)
+  
+		if (str[i]>='0' && str[i]<='9')
+    
+		c++;
+    
+	printf("%d",c);
+  
+	return 0;
+  
+}
