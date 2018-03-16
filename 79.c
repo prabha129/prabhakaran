c7328b85336b40f31cb1ae3c1d693e50b0cbd432
prabha129 <35652743+prabha129@users.noreply.github.com>
2018-03-16 16:11:59 +0530

    Update 79.c

--- 79.c
+++ 79.c
@@ -3,17 +3,20 @@
 int main()
 {
     char data[50];
-    int l,i,s1,s2,s3,t;
-    s1=s2=s3=0;
+    int l,i,r1,r2,r3,t;
+    
+    r1=r2=r3=0;
     scanf("%s",&data);
     l = strlen(data);
     for(t=0;t<l;t++){
         i = (int)data[t];
-        if(i>=97 && i <= 122) s1 = 1;
-        if(i>=48 && i <= 57) s2 = 1;
-        if(i>=65 && i <= 90) s1 = 1;
+        if(i>=97 && i <= 122) r1 = 1;
+        if(i>=48 && i <= 57) r2 = 1;
+        if(i>=65 && i <= 90) r1 = 1;
     }
-    if(s1+s2 == 2) printf("yes");
+    if(r1+r2 == 2) printf("yes");
     else printf("no");
     return 0;
 }
+
+
